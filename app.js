325d03b142df3cd6a8fca88b18a34b1e5ba6f5b7
Kumar Tatsat <rvk8878@gmail.com>
2022-03-27 00:11:43 +0530

    JSON payload parsing now working

--- app.js
+++ app.js
@@ -2,33 +2,31 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-let cors = require('cors');
+const cors = require('cors');
 
-app.use(cors({origin: 'http://localhost:5500'}));
+// app.use(cors({origin: '*'}));
 
-// Setting path for public directory
-// const static_path = path.join(__dirname, "public");
-// app.use(express.static(static_path));
-// app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
 
-// app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', '*');
 
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 
-//   // Pass to next layer of middleware
-//   next();
-// });
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send('this is the root page, lmao');
@@ -36,12 +34,10 @@ app.get("/", (req, res) => {
 
 // Handling request
 app.post("/request", (req, res) => {
-  // const data = JSON.parse(req.body);
+  console.log(`client sent: ${Object.keys(req.body)}`);
 
-  console.log(`unparsed: ${req.body}`);
-
-  res.status(200).send({
-    essay: 'request ka essay',
+  const response = {
+    essay: req.body.essay,
     lead: ['lead?'],
     position: ['pos1', 'post2'],
     claim: ['claim 1', 'claim 2'],
@@ -49,7 +45,9 @@ app.post("/request", (req, res) => {
     rebuttal: ['rebuttal 1'],
     evidence: ['evidence 1', 'evidence 2', 'evidence 3'],
     conclusion: ['conclusion is hagga']
-  });
+  };
+
+  res.status(200).json(response);
 })
 .listen(port, () => {
   console.log(`server is running at ${port}`);
