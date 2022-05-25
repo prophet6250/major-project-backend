f97dd37bd31f4dc877073996e47e2ca0ccbbc177
Kumar Tatsat <rvk8878@gmail.com>
2022-05-25 08:59:30 +0530

    phase/beta complete

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 const express = require("express");
-const { setTimeout } = require("timers/promises");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// remember this thing. this adds CORS headers to all incoming requests
 app.use((req, res, next) => {
-  
-  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
-  
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', '*');
-  
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', '*');
-  
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  
-  // Pass to next layer of middleware
   next();
 });
 
-// convert incoming data into json using express's body-parser thingy
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/request', (req, res) => {
   const FILE_NAME = '12.txt';
   const MODEL_NAME = req.body.modelName;
-  const { unlink, readFileSync, writeFileSync } = require('fs');
+  const { readFileSync, writeFileSync } = require('fs');
   const { spawnSync } = require('child_process');
 
   console.log(req.body);
@@ -49,11 +35,6 @@ app.post('/request', (req, res) => {
   spawnSync('bash', ['query.sh', MODEL_NAME], { cwd: './models' });
 
   const data = readFileSync('./models/output.json', { encoding: 'utf-8'});
-
-  unlink('models/output.json', (err) => {
-    if (err) throw err;
-    console.log('models/output.json deleted successfully');
-  });
 
   res.send(data);
 });
