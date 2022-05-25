151ddd6d76b7e0b224883134ab2032623561e7d0
Kumar Tatsat <rvk8878@gmail.com>
2022-05-25 21:25:33 +0530

    clean up backend code further

--- app.js
+++ app.js
@@ -16,23 +16,28 @@ app.use(express.json());
 app.post('/request', (req, res) => {
   const FILE_NAME = '12.txt';
   const MODEL_NAME = req.body.modelName;
-  const { readFileSync, writeFileSync } = require('fs');
-  const { spawnSync } = require('child_process');
 
   console.log(`STEP 1: incoming request to run model ${MODEL_NAME}...`);
 
+  const { readFileSync, writeFileSync } = require('fs');
+  const { spawnSync } = require('child_process');
+
   writeFileSync(`./models/test/${FILE_NAME}`, req.body.essay, { flag: 'w+' });
-  console.log(`STEP 2: ${FILE_NAME} successfully written...`);
+  console.log(`STEP 2: input file ${FILE_NAME} successfully written...`);
   
   /*
-  DESIGN NOTE: I am not sanitising MODEL_NAME, because... laziness. This is a security flaw. 
-  Any input containing shell metacharacters may be used to trigger arbitrary command execution
+    DESIGN NOTE: I am not sanitising MODEL_NAME, because... laziness. This is a security flaw. 
+    Any input containing shell metacharacters may be used to trigger arbitrary command execution
   */
-  spawnSync('bash', ['query.sh', MODEL_NAME], { cwd: './models' });
+  let runModel = spawnSync('bash', ['query.sh', MODEL_NAME], { cwd: './models' });
+  
+  console.log('\n########## MODEL OUTPUT START #########');
+  console.log(runModel.stdout.toString());
+  console.log('########## MODEL OUTPUT END #########\n');
   console.log(`STEP 3: ${MODEL_NAME} ran successfully`)
 
   const data = readFileSync('./models/output.json', { encoding: 'utf-8'});
-  console.log('STEP 4: output.json read successfully. sending response to client...');
+  console.log('STEP 4: output.json read successfully. sending response to client...\n\n');
   res.send(data);
 });
 
